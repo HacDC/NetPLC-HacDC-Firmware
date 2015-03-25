8af4b0e6c42219e60a5ae838b92315c4abb9f27b
mirage335 <spamfreemirage335@gmail.com>
2015-03-25 04:48:12 +0000

    Webduino support tested.

--- src/WebUI/WebUI.ino
+++ src/WebUI/WebUI.ino
@@ -1,65 +1,56 @@
-//http://arduino.cc/en/tutorial/blink
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
-
 #include <avr/io.h>
 #include "prescaler.h"
 
-#include <EtherCard.h>
+#include "SPI.h"
+#include "UIPEthernet.h"
+#include "WebServer.h"
 
 // ethernet interface mac address, must be unique on the LAN
 //Xen prefix 00:16:3E, random suffix.
-static byte mymac[] = { 0x00,0x16,0x3E,0x72,0x6A,0x48 };
+static uint8_t mac[] = { 0x00, 0x16, 0x3E, 0x72, 0x6A, 0x48 };
 
-byte Ethernet::buffer[1024];
-BufferFiller bfill;
-
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-int led = A5;
-
-
-int xyz = 0;
+/* CHANGE THIS TO MATCH YOUR HOST NETWORK. Most home networks are in
+* the 192.168.0.XXX or 192.168.1.XXX subrange. Pick an address
+* that's not in use and isn't going to be automatically allocated by
+* DHCP from your router. */
+static uint8_t ip[] = { 192, 168, 50, 123 };
 
 
-static word homePage() {
-  long t = trueMillis() / 1000;
-  word h = t / 3600;
-  byte m = (t / 60) % 60;
-  byte s = t % 60;
-  bfill = ether.tcpOffset();
-  bfill.emit_p(PSTR(
-    "HTTP/1.0 200 OK\r\n"
-    "Content-Type: text/html\r\n"
-    "Pragma: no-cache\r\n"
-    "\r\n"
-    //"<meta http-equiv='refresh' content='1'/>"
-    "<title>ENC28J60 Web Interface - HacDC NetPLC</title>"
-    "<p>Uptime: $D$D:$D$D:$D$D</p>"
-    "<p>Ready to authorize: <form action='' method='get'><input type='text' name='name' value='anon'><input type='submit' value='Submit'></forum></p>"
-    "<p>Internal LED: <form action='' method='get'><button name='LED' type='submit' value='ON'>ON</button><button name='LED' type='submit' value='OFF'>OFF</button></form></p>"
-    ""),
-      h/10, h%10, m/10, m%10, s/10, s%10);
-  return bfill.position();
+/* This creates an instance of the webserver. By specifying a prefix
+ * of "", all pages will be at the root of the server. */
+#define PREFIX ""
+WebServer webserver(PREFIX, 80);
+
+/* commands are functions that get called by the webserver framework
+ * they can read any posted data from client, and they output to the
+ * server to send data back to the web browser. */
+void helloCmd(WebServer &server, WebServer::ConnectionType type, char *, bool)
+{
+	/* this line sends the standard "we're all OK" headers back to the
+		 browser */
+	server.httpSuccess();
+
+	/* if we're handling a GET or POST, we can output our data here.
+		 For a HEAD request, we just stop after outputting headers. */
+	if (type != WebServer::HEAD)
+	{
+		/* this defines some HTML text in read-only memory aka PROGMEM.
+		 * This is needed to avoid having the string copied to our limited
+		 * amount of RAM. */
+		P(helloMsg) = "<h1>Hello, World!</h1>";
+
+		/* this is a special form of print that outputs from PROGMEM */
+		server.printP(helloMsg);
+	}
 }
 
-void processData(char* pair) {
-	
-	char* value = strchr((char *)pair, '=') + 1;
-	
-	Serial.println((char *)pair);
-	//Serial.println((char *)value);
-	
-}
-
-void setup() {
+void setup()
+{
 	setClockPrescaler(0x1);  //<8MHz for reliable 3.3V operation. >1MHz recommended.
-	
+
 	Serial.begin(115200);  // Maybe a lot faster over native USB.
+	
+	int led = A5;
 	
 	// initialize the digital pin as an output.
 	pinMode(led, OUTPUT);
@@ -68,77 +59,30 @@ void setup() {
 	digitalWrite(led, LOW);
 	trueDelay(100);
 	digitalWrite(led, HIGH);
-	
+
 	trueDelay(1500);
 	
-	  Serial.println("\n[getDHCPandDNS]");
-  
-	if (ether.begin(sizeof Ethernet::buffer, mymac) == 0) 
-	Serial.println( "Failed to access Ethernet controller");
-
-	if (!ether.dhcpSetup())
-	Serial.println("DHCP failed");
-
-	ether.printIp("My IP: ", ether.myip);
-	// ether.printIp("Netmask: ", ether.mymask);
-	ether.printIp("GW IP: ", ether.gwip);
-	ether.printIp("DNS IP: ", ether.dnsip);
-}
-
-void loop() {
-	word len = ether.packetReceive();
-	word pos = ether.packetLoop(len);
 	
-	if (pos)  {// check if valid tcp data is received
-		
-		if (strncmp("GET /?", (char *) Ethernet::buffer + pos, 6) == 0) {
-			
-			Serial.println("ROOT GET DETECTED! #####");
-			
-			byte paramBuffer[35];
-			
-			strncpy((char *) paramBuffer, (char *) Ethernet::buffer + pos + 6, 30);
-			
-			
-			char* pair = strtok((char *) paramBuffer, "&");
+	/* initialize the Ethernet adapter */
+	Ethernet.begin(mac, ip);
 
-			while( pair != NULL ) {
-				
-				processData(pair);
-				
-				pair = strtok(NULL, "&");
-			}
-			
-		
-			//http://192.168.50.118/?LED=OFF
-			/*
-			if (strstr((char *)Ethernet::buffer + pos, "LED=OFF") != 0) {
-				digitalWrite(led, LOW);
-			}
-			else if (strstr((char *)Ethernet::buffer + pos, "LED=ON") != 0) {
-				digitalWrite(led, HIGH);
-			}
-			*/
-			
-			//Serial.println((char *)name);
-			//Serial.println((char *)value);
-		}
-		
-		Serial.println((char *)Ethernet::buffer + pos);
-		
-		ether.httpServerReply(homePage()); // send web page data
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
+	/* setup our default command that will be run when the user accesses
+	 * the root page on the server */
+	webserver.setDefaultCommand(&helloCmd);
+
+	/* run the same command if you try to load /index.html, a common
+	 * default page name */
+	webserver.addCommand("index.html", &helloCmd);
+
+	/* start the webserver */
+	webserver.begin();
 }
 
+void loop()
+{
+	char buff[64];
+	int len = 64;
+
+	/* process incoming connections one at a time forever */
+	webserver.processConnection(buff, &len);
+}
